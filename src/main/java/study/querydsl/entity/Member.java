b0ec46d70161ed5cbe5d6848d0d1e8d022ad883d
miyeon396 <miyeon396@naver.com>
2024-05-12 21:58:24 +0900

    ch6. 스프링 데이터 JPA 리포로 변경
    - 멤버객체가 다르다길래 equalsAndHashCode 추가

--- src/main/java/study/querydsl/entity/Member.java
+++ src/main/java/study/querydsl/entity/Member.java
@@ -7,6 +7,7 @@ import lombok.*;
 @Getter @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(of = {"id", "username", "age"})
+@EqualsAndHashCode
 public class Member {
 
     @Id @GeneratedValue
---
+++ src/main/java/study/querydsl/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package study.querydsl.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import study.querydsl.entity.Member;
+
+import java.util.List;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    //select m from Member m Where m.username = ? 이게 매핑되면서 실행 될 것
+    List<Member> findByUsername(String username);
+}
